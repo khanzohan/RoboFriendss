9b3def868d31bf2dfb5733634f70fb04169aa522
khanzohan <zoho12@live.com>
2019-10-27 15:24:06 -0400

    Updates

---
+++ precache-manifest.12da2b4018bab27b43585403fe1736fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8fb64953b0250cf092edd61abf0ed94",
+    "url": "/khanzohan/RoboFriendss/index.html"
+  },
+  {
+    "revision": "2fc4841dd2a2e8b98e5b",
+    "url": "/khanzohan/RoboFriendss/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a7e712eea04b60c71297",
+    "url": "/khanzohan/RoboFriendss/static/css/main.354f3c03.chunk.css"
+  },
+  {
+    "revision": "2fc4841dd2a2e8b98e5b",
+    "url": "/khanzohan/RoboFriendss/static/js/2.eb08a8a6.chunk.js"
+  },
+  {
+    "revision": "a7e712eea04b60c71297",
+    "url": "/khanzohan/RoboFriendss/static/js/main.5429c75b.chunk.js"
+  },
+  {
+    "revision": "0d8379f7374d3b419ea6",
+    "url": "/khanzohan/RoboFriendss/static/js/runtime-main.e0d88648.js"
+  }
+]);
